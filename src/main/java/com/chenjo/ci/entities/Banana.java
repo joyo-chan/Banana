ceb1bf1c55b629146d9012d95f4b1aee1fa78ec3
jonathan.n.chen <jonathan.n.chen@gmail.com>
2019-05-14 18:40:50 +0800

    my 2nd commit

--- src/main/java/com/chenjo/ci/entities/Banana.java
+++ src/main/java/com/chenjo/ci/entities/Banana.java
@@ -1,7 +1,7 @@
 package com.chenjo.ci.entities;
 
 public class Banana {
-	private String appName = "Good banana";
+	private String appName = "Good banana ||| ";
 
 	public String getAppName() {
 		return appName;
